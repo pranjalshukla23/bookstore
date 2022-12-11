bf29629b4bffdbcbef47d16f7082b45e9578d63f
pranjalshukla23 <62809277+pranjalshukla23@users.noreply.github.com>
2022-12-11 22:58:28 +0530

    resolved server side rendering issue by adding logic to fetch data from db directly inside getServerSideProps

--- pages/books/[id].js
+++ pages/books/[id].js
@@ -1,17 +1,38 @@
 import { BookDetail } from "../../components/BookDetail";
 import { getBookById } from "../../api-helpers/frontend/utils";
+import { Book } from "../../api-helpers/models/Book";
 
-export default function Book({ book, id }) {
+export default function getBook({ book, id }) {
   return <BookDetail fetchedBook={book} id={id} />;
 }
 
 export const getServerSideProps = async (context) => {
+  console.log("id", context.query.id);
   const { id } = context.query;
 
-  //get book by id
-  const book = await getBookById(id);
+  let book;
 
-  //console.log(book);
+  try {
+    //find book by id in db
+    book = await Book.findById(id);
+
+    //console.log("book is", book);
+  } catch (err) {
+    console.log("error", err);
+    return {
+      props: {},
+    };
+  }
+  if (!book) {
+    console.log("book not found");
+    return {
+      props: {},
+    };
+  }
+
+  const text = JSON.stringify(book);
+
+  book = JSON.parse(text);
 
   return {
     props: {
--- pages/books/index.js
+++ pages/books/index.js
@@ -1,12 +1,32 @@
 import { getAllBooks } from "../../api-helpers/frontend/utils";
 import { BookList } from "../../components/BookList";
+import { Book } from "../../api-helpers/models/Book";
 
 export default function Books({ books }) {
   return <BookList data={books} />;
 }
 
 export const getServerSideProps = async (context) => {
-  const books = await getAllBooks();
+  let books;
+
+  try {
+    //get all books from db
+    books = await Book.find();
+  } catch (err) {
+    return new Error(err);
+  }
+
+  if (!books) {
+    return {
+      props: {
+        books: [],
+      },
+    };
+  }
+
+  const text = JSON.stringify(books);
+
+  books = JSON.parse(text);
 
   return {
     props: {
